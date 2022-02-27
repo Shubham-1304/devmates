7709560fc61f29a918ab6a20256a08f84609d3fa
shubh123 <shubh@example.com>
2022-02-27 12:24:56 +0530

    initial commit

--- course1/settings.py
+++ course1/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure--+&5afz!)lqe&^hr!&!lv@reyy$s0j#7x4vzpopt416dwh^h@d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','devmates.herokuapp.com']
+ALLOWED_HOSTS = ['devmates.herokuapp.com']
 
 
 # Application definition
